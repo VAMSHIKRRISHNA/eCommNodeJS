d578a5dfb317cdeb127655e48c0359262e9d46ca
VAMSHIKRRISHNA <vamshinandyala@gmail.com>
2020-10-02 02:25:39 +1000

    Adding tests with mocha chai and chai-http

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  return res.send('Hello World')
+app.get('/', (req, res) => {
+    try {
+        return res.send('Hello World')
+    } catch (error) {
+        return res.status(500).send('Hello World')
+    }
 })
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Server is running on port : 3000')
 })
+
+module.exports = app
---
+++ tests/serverTest.js
@@ -0,0 +1,19 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const app = require('../index')
+
+// config chai
+chai.use(chaiHttp)
+chai.should()
+
+describe('Test server connectivity', () => {
+    it('Should return 200', (done) => {
+        chai.request(app)
+            .get('/')
+            .end((err, res) => {
+                res.should.have.status(200)
+                res.body.should.be.a('object')
+                done()
+            })
+    })
+})

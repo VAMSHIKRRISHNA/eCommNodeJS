d7d28d5f1a761dfeef35ff6637de1140b455691c
VAMSHIKRRISHNA <vamshinandyala@gmail.com>
2020-10-02 01:33:29 +1000

    Adding lint module

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function(req, res){
-    return res.send('Hello World');
-});
+app.get('/', function (req, res) {
+  return res.send('Hello World')
+})
 
-app.listen(3000, function() {
-    console.log('Server is running on port : 3000')
-});
+app.listen(3000, function () {
+  console.log('Server is running on port : 3000')
+})
